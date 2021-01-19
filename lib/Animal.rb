e9ffd4a851e57a99157b27f705f4eb60c3e38b2c
Erik Huang <erikhuangdrums@gmail.com>
2021-01-19 07:31:42 -0800

    done

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,31 @@
 class Animal
+    @@all = []
 
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(species)
+        self.all.select { |animal| animal.species == species }
+    end
+
+    attr_reader :species, :nickname
+    attr_accessor :weight, :zoo
+
+    def initialize(species, weight, nickname)
+        @species, @weight, @nickname = species, weight, nickname
+        save
+    end
+
+    def save
+        self.class.all << self
+    end
+
+    def join_zoo(zoo)
+        self.zoo = zoo
+    end
+
+    def zoo
+        self.zoo
+    end
 end
